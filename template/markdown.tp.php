b3896995e8434ac231dabf483bbbdeb2de0c08da
SnailCoder <shihai.li@live.com>
2018-02-11 19:21:25 +0800

    新窗口打开外链

--- template/markdown.tp.php
+++ template/markdown.tp.php
@@ -11,10 +11,14 @@
 <article class="markdown-body">
     <?php echo $body; ?>
 </article>
+<script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
 <script src="//cdn.bootcss.com/highlight.js/9.12.0/highlight.min.js"></script>
 <script src="//cdn.bootcss.com/highlight.js/9.12.0/languages/go.min.js"></script>
 <script src="//cdn.bootcss.com/highlightjs-line-numbers.js/2.1.0/highlightjs-line-numbers.min.js"></script>
 <script type="text/javascript">
+    $('a[href^="http"]').each(function() {
+        $(this).attr('target', '_blank');
+    });
     hljs.initHighlightingOnLoad();
     hljs.initLineNumbersOnLoad();
 </script>
